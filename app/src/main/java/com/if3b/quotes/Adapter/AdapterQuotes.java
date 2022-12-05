e423010bcd7e439e48d4b8b1b611787f5aaae4ee
mrvldo <mariorivaldo86@gmail.com>
2022-12-05 11:02:41 +0700

    commit

--- app/src/main/java/com/if3b/quotes/Adapter/AdapterQuotes.java
+++ app/src/main/java/com/if3b/quotes/Adapter/AdapterQuotes.java
@@ -1,6 +1,7 @@
 package com.if3b.quotes.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -26,17 +27,20 @@ public class AdapterQuotes extends RecyclerView.Adapter<AdapterQuotes.ViewHolder
     @NonNull
     @Override
     public ViewHolderQuotes onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View varView = LayoutInflater.from(ctx).inflate(R.layout.item_quotes, parent, false);
+        return new ViewHolderQuotes(varView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderQuotes holder, int position) {
-
+        QuotesModel QM = listQuotes.get(position);
+        holder.tvQuotes.setText(QM.getText());
+        holder.tvAuthor.setText(QM.getAuthor());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listQuotes.size();
     }
 
     public class ViewHolderQuotes extends RecyclerView.ViewHolder {
